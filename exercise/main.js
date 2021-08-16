95a26096050f28d889e910ad759cde7313b0a2d6
FZSZ <iinthworldon@gmail.com>
2021-08-16 16:20:21 -0300

    shadow added to content

--- exercise/main.js
+++ exercise/main.js
@@ -4,19 +4,23 @@ window.onload = () => {
   let button = document.querySelector('.share-button');
   let share = document.querySelector('.share');
 
+  const result = (to) => {
+    share.classList[to]('active');
+    button.classList[to]('active');
+    path.classList[to]('active');
+    
+    if (path.classList == 'active') {
+      path.setAttribute('fill', '#ecf2f8');
+    } else {
+      path.setAttribute('fill', '#6E8098');
+    }
+  }
+
   document.addEventListener('click', e => {
     if (e.target !== button) {
-      return;
+      result('remove'); 
     } else {
-      share.classList.toggle('active');
-      button.classList.toggle('active');
-      path.classList.toggle('active');
-
-      if (path.classList == 'active') {
-        path.setAttribute('fill', '#ecf2f8');
-      } else {
-        path.setAttribute('fill', '#6E8098');
-      }
+      result('toggle');
     }
   });
 }
